5da4e4c310c984da9ea6299ba4a2bb4126c452a9
lamtrieuvi.it@gmail.com <lamtrieuvi.it@gmail.com>
2021-07-17 18:41:36 +0700

    update

---
+++ QuanLySach/ViewModel/SachViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuanLySach.ViewModel
+{
+    public class SachViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string AuthorName { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
